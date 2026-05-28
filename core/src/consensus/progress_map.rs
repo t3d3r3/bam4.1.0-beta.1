3380ae12203295cf068f43a5ecb6f1f2c8728c20
mihaitdr <mihaitdr@gmail.com>
2026-05-28 18:10:44 +0300

    patch 4.1

--- core/src/consensus/progress_map.rs
+++ core/src/consensus/progress_map.rs
@@ -204,6 +204,7 @@ pub struct ForkStats {
     pub is_locked_out: bool,
     pub voted_stakes: VotedStakes,
     pub duplicate_confirmed_hash: Option<Hash>,
+    pub is_mostly_confirmed: bool,
     pub computed: bool,
     pub lockout_intervals: LockoutIntervals,
     pub bank_hash: Option<Hash>,
@@ -388,6 +389,11 @@ impl ProgressMap {
     pub fn set_duplicate_confirmed_hash(&mut self, slot: Slot, hash: Hash) {
         let slot_progress = self.get_mut(&slot).unwrap();
         slot_progress.fork_stats.duplicate_confirmed_hash = Some(hash);
+    }
+
+    pub fn set_mostly_confirmed_slot(&mut self, slot: Slot) {
+        let slot_progress = self.get_mut(&slot).unwrap();
+        slot_progress.fork_stats.is_mostly_confirmed = true;
     }
 
     pub fn is_duplicate_confirmed(&self, slot: Slot) -> Option<bool> {
